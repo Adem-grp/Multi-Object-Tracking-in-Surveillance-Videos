9f74baf167e5e74e54991025444bb1639d7eb97f
Adem-grp <109536482+Adem-grp@users.noreply.github.com>
2025-11-12 15:43:54 +0000

    convert_mot_to_yolo function is added

--- Arrange_File_Paths_For_Yolov8n.py
+++ Arrange_File_Paths_For_Yolov8n.py
@@ -3,6 +3,7 @@ import shutil
 import glob
 import random
 import pandas as pd
+import cv2
 
 gmot_path = "C:/Users/USER/PycharmProjects/Multi-Object-Tracking-in-Surveillance-Videos/GenericMOT_JPEG_Sequence"
 yolo_path = "C:/Users/USER/PycharmProjects/Multi-Object-Tracking-in-Surveillance-Videos/gmot_yolo"
@@ -29,6 +30,43 @@ def copy_images(images, spl1t):
         dst = os.path.join(yolo_path, spl1t, "images", f_name)
         shutil.copy(image, dst)
 
-copy_images(train_images,"train")
-copy_images(valid_images,"val")
 
+copy_images(train_images, "train")
+copy_images(valid_images, "val")
+
+
+def convert_mot_to_yolo(seq_folder, out_folder):
+    """ seq=GMOT sequence folder
+        out= yolo labels folder
+    """
+    gt_file = os.path.join(seq_folder, "gt", "gt.txt")
+    if not os.path.exists(gt_file):
+        return
+    df = pd.read_csv(gt_file, header=None)
+    df.columns = ["frame", "id", "x", "y", "w", "h", "conf", "cls", "vis"]
+    grouped = df.groupby("frame")
+    img_folder = os.path.join(seq_folder, "imag1")
+    images = glob.glob(os.path.join(img_folder, "*.jpg"))
+    for img in images:
+        f_name = os.path.basename(img)
+        frame_no = int(os.path.splitext(f_name)[0])
+        dst = os.path.join(out_folder, f_name)
+        img = cv2.imread(img)
+        H, W = img.shape[:2]
+
+        if frame_no not in grouped.groups:
+            continue
+        rows = grouped.get_group(frame_no)
+
+        yolo_lines = []
+        for _, row in rows.iterrows():
+            x_center = (row["x"] + row["w"] / 2) / W
+            y_center = (row["y"] + row["h"] / 2) / H
+            width = row["w"] / W
+            height = row["h"] / H
+            class_id=int(row["cls"])
+            yolo_lines.append(f"{class_id} {x_center:.6f} {y_center:.6f} {width:.6f} {height:.6f}")
+
+            out_path= os.path.join(out_folder,os.path.splitext(f_name)[0]+".txt")
+            with open(out_path, "w") as f:
+                f.write("\n".join(yolo_lines))
