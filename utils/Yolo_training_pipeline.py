a9d89e5a6209ad50a5b7bfdf59c75e766209a2b2
Adem-grp <109536482+Adem-grp@users.noreply.github.com>
2026-04-20 06:36:51 +0100

    fix: add tuning function in Yolo_training_pipeline.py

--- utils/Yolo_training_pipeline.py
+++ utils/Yolo_training_pipeline.py
@@ -25,6 +25,7 @@ runName = "full_finetune"  #  1 is for inital without hyperparameter tuning much
 eval_results_path = "YOLO_training_results/eval_GMOT_ONLY_results_1.json"  # change in each evaluation
 batchSize = 32
 number_of_epochs = 50
+tune_run = "runs/GMOT_only_1/tune"
 
 
 def train():
@@ -44,34 +45,7 @@ def train():
         # set pretrained true for training the best model again
         optimizer='auto',
         seed=42,
-        # the ones below are explanation of hyper parameters
-        # after the initial run check the results and apply the necessaryones
-        # each I wrote their explanations if not enough check ultralytics again
 
-        #cos_lr=True,
-        #close_mosaic=3, # disable data augmentation in last 3 epochs for traning stabilisation
-        # if I want to reduce memory usage I need to set amp=True
-        # freeze (int): freezes first N layers or specified layers by index
-        # it reduces the number of trainable parameters it is useful for
-        # fine-tuning and transfer learning
-        # lr0(float) = inital learning rate  adjusting this is important
-        # lrf (float)= final learning rate as a fraction of the inital rate (lr0*lrf) used in conjunction
-        # with schedulers to adjust the learning rate over time
-        # momentum(float)= it is for SGD or beta1 for adam optmisors for influencing the
-        # incorporation of past gradients in the current update
-        # weight_decay(float): penalise large weights to prevent overfittin like 5e-5
-        # warmup_epochs(float): e.g 3 . for learning rate warm up gradually increasing the learning rate from a low val
-        # to the lr0 to stabilise training early on
-        # warmup_momentum (float): test this but I guess using momentum won't be that much of a big deal so
-        # warmup_bias_lr(float): learning rate for bias parameters during the warmup phase helping stablise model training
-        # in the initial epochs
-        # box (float) : weight of the box loss componenet in the loss function influencing how much emphasis is placed
-        # on accuractely predicting the bounding box coordinates
-        # cls (float): weight of the class loss component in the loss function influencing how much emphasis is placed
-        # on accurately predicting the class labels
-        # cls_pw (float): Power for class weighting to handle class imbalance using inverse class frequency.
-        # 0.0 disables class weighting, 1.0 applies full inverse frequency weighting. Values between 0 and 1 provide partial weighting.
-        # there are a few more but first deal with the ones above
 
     )
     return results
@@ -123,7 +97,29 @@ def validate(best_weights_path):
     print(f"FPS: {metrics['real_time_metrics']['estimated_fps']} | mAP50: {metrics['accuracy_metrics']['mAP50']}")
 
 
+def tune():
+    model = YOLO("yolo11n.pt")
+    model.tune(
+        data=yamlPath,
+        epochs=30,
+        iterations=100,
+        optimizer="auto",
+        plots=True,
+        save=True,
+        project=tune_run,
+        name="tune_baseline",
+    )
+    print(f"Tuning complete. Best hyperparameters saved to {tune_run}/tune_baseline/")
+    print("Use best_hyperparameters.yaml in your training runs.")
+
+
+
+
+
+
 if __name__ == "__main__":
-    results = train()
-    best_weights = f"{train_run}/{runName}/weights/best.pt"
-    validate(best_weights_path=best_weights)
+    print("Cuda available: ", torch.cuda.is_available())
+    tune()
+    #results = train()
+    #best_weights = f"{train_run}/{runName}/weights/best.pt"
+    #validate(best_weights_path=best_weights)
