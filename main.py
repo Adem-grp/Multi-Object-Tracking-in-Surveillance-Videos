cdbb0c8d95457b6c69ef3187b04bde5391b9f000
Adem-grp <109536482+Adem-grp@users.noreply.github.com>
2026-07-21 06:58:41 +0100

    removing temporary and unnecessary files

--- main.py
+++
@@ -1,9 +0,0 @@
-"""
-Avenue_yolo will stay as I will use CVAT on test but when I do the traning and what not I will use the bridge_yolo's
-avenue since they are labeled via yolo11n.pt already
-
-I will need to do the same for UCSD and ShanghaiTech's Subway one as well
-
-
-
-"""
